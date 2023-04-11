f55255a66900002bc10cbb61a242fa46e089fec5
lucaruboni <lucaruboni@gmail.com>
2023-04-11 16:32:48 +0200

    ticket

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,32 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-  
+        url: "https://flynn.boolean.careers/exercises/api/random/mail",
+        myData:null,
+        email: [
+
+        ]
     }
   },
 
   mounted(){
+    axios
+    .get(this.url)
+    .then(response => {
+        
 
+    for (let i = 0; i < 10; i++) {
+        this.myData = response.data
+        let element = response.data.response
+        console.log(element)
+    }
+
+  /*       for (let i = 0; i < 10; i++) {
+            this.myData += response.data.response
+            console.log(response.data.response)
+            
+        } */
+    })
 
   }
 }).mount('#app')
